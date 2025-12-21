35b5f83174fb80df5164a3ddd58adaed86b96c3e
Phosphorite <dew@phosphorite.art>
2025-12-21 01:45:38 -0800

    expose getPosts

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,5 +1,5 @@
 import { MongoCache, type MongoDbCacheConfig } from './cache.ts';
-import { PostsClient } from '../util/posts.ts';
+import { PostsClient, type Post } from '../util/posts.ts';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import type { ICache } from '../util/cache.ts';
 
@@ -36,8 +36,8 @@ function makeCache(opts: CacheOptions | undefined): ICache | null {
 	}
 }
 
-export function configure(config: Configuration): RequestHandler {
-	const client = new PostsClient({
+function makeClient(config: Configuration): PostsClient {
+	return new PostsClient({
 		bskyPassword: config.bskyPassword,
 		bskyDid: config.bskyDid,
 		bskyUsername: config.bskyUser,
@@ -46,11 +46,29 @@ export function configure(config: Configuration): RequestHandler {
 			maxAgeMs: config.cache?.refreshPeriodMs
 		}
 	});
+}
 
-	return async () => {
-		return json(await client.getPosts(config.maxPosts ?? 9), {
+export class PostHandler {
+	#client: PostsClient;
+	#config: Configuration;
+
+	constructor(config: Configuration) {
+		this.#config = config;
+		this.#client = makeClient(config);
+	}
+
+	get handler(): RequestHandler {
+		return this.#handler;
+	}
+
+	async getPosts(): Promise<Post[]> {
+		return await this.#client.getPosts(this.#config.maxPosts ?? 9);
+	}
+
+	#handler: RequestHandler = async () => {
+		return json(await this.getPosts(), {
 			headers: {
-				'Cache-Control': `public, max-age=${config.cacheControl?.maxAge ?? 600}, stale-while-revalidate=${config.cacheControl?.staleWhileRevalidate ?? 86400}, stale-if-error=${config.cacheControl?.staleIfError ?? 86400}`
+				'Cache-Control': `public, max-age=${this.#config.cacheControl?.maxAge ?? 600}, stale-while-revalidate=${this.#config.cacheControl?.staleWhileRevalidate ?? 86400}, stale-if-error=${this.#config.cacheControl?.staleIfError ?? 86400}`
 			}
 		});
 	};
